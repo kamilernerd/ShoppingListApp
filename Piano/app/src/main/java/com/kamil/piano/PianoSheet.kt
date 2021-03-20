c292089f23e7fa70aa11a0ed3fb7d2038bd79f24
Kamil <kamil@indicosys.com>
2021-03-20 14:28:11 +0100

    Remove old code from others apps

--- Piano/app/src/main/java/com/kamil/piano/PianoSheet.kt
+++
@@ -1,7 +0,0 @@
-package com.kamil.piano
-
-data class PianoSheet(val note:String, val startTimeOfNote:Long, val noteTimeDuration:Long){
-    override fun toString(): String {
-        return "Note: $note , Started playing at: $startTimeOfNote , Lasted for: $noteTimeDuration"
-    }
-}
--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/util/Time.kt
+++
@@ -1,9 +0,0 @@
-package no.uia.ikt205.pomodoro.util
-
-fun millisecondsToDescriptiveTime(ms:Long):String {
-    val seconds = (ms / 1000)  % 60
-    val minutes = (ms / (1000 * 60) % 60)
-    val hours = (ms / (1000 * 60 * 60) % 24)
-
-    return String.format("%02d:%02d:%02d", hours, minutes, seconds);
-}
