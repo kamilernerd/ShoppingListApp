73f9f88c1769c294672617071b2d3ce2d9cfb916
Kamil <kamil@indicosys.com>
2021-03-24 15:24:25 +0100

    Add firebase

--- app/src/main/java/com/kamil/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/kamil/shoppinglist/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kamil.shoppinglist.databinding.ActivityMainBinding
 import com.kamil.shoppinglist.dialogs.AddNewListDialogFragment
 import com.kamil.shoppinglist.viewmodels.ListsCollectionViewModel
@@ -12,13 +15,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     private lateinit var binding: ActivityMainBinding
     private var listsCollectionViewModel: ListsCollectionViewModel = ListsCollectionViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+
+        if (currentUser == null) {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        } else {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        }
+
+
 
         binding.listCollection.layoutManager = LinearLayoutManager(this)
         binding.listCollection.adapter = ListCollectionAdapter(listsCollectionViewModel)
