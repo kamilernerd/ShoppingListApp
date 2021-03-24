736d30a8f250fd60e955d480b5aaea52b402f277
Kamil <kamil@indicosys.com>
2021-03-24 22:52:48 +0100

    Add basic auth fragment and small testing function

--- app/src/main/java/com/kamil/shoppinglist/AuthFragment.kt
+++ app/src/main/java/com/kamil/shoppinglist/AuthFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kamil.shoppinglist.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
 
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
     }
 
     override fun onCreateView(
